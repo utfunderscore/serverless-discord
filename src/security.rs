70be0a90c1275b26f545838d48034f500bb10572
utfunderscore <83981186+utfunderscore@users.noreply.github.com>
2025-05-13 16:08:27 +0100

    fix: Export verifying key instead of signing key for tests

--- src/security.rs
+++ src/security.rs
@@ -1,7 +1,5 @@
 use ed25519_dalek::{Signature, Signer, SigningKey, Verifier, VerifyingKey};
 use hex::FromHex;
-use hex::ToHex;
-use rand::rngs::OsRng;
 
 
 /// Verifies a Discord request by checking the provided signature against the message.
@@ -51,18 +49,40 @@ pub fn verify_discord_request(
         .map_err(|_| String::from("Request body does not match signature"))
 }
 
+pub fn generate_signature(signing_key: &SigningKey, body: &str, timestamp: &str) -> Signature {
+    let message = [timestamp.as_bytes(), body.as_bytes()].concat();
+    signing_key.sign(message.as_slice())
+}
+
 #[cfg(test)]
 mod tests {
+    use ed25519_dalek::ed25519::signature::Keypair;
+    use ed25519_dalek::SigningKey;
+    use rand::rngs::OsRng;
+    use crate::security::generate_signature;
+    use hex::ToHex;
 
     #[test]
+    fn generate_discord_signature() {
+        let body = r#"{"app_permissions":"562949953601536","application_id":"1096551423958855810","attachment_size_limit":524288000,"authorizing_integration_owners":{},"entitlements":[],"id":"0000000001","token":"example_token","type":1,"user":{"avatar":"c6a249645d46209f337279cd2ca998c7","avatar_decoration_data":null,"bot":true,"clan":null,"collectibles":null,"discriminator":"0000","global_name":"Discord","id":"643945264868098049","primary_guild":null,"public_flags":1,"system":true,"username":"discord"},"version":1}"#;
+        let timestamp = "1746297636";
+        let signing_key = SigningKey::generate(&mut OsRng);
+        
+        let signature = generate_signature(&signing_key, body, timestamp);
+        println!("Verifying Key: {}", signing_key.verifying_key().to_bytes().encode_hex::<String>());
+        println!("Signature: {}", signature.to_bytes().encode_hex::<String>());
+    }
+    
+    #[test]
     fn test_verify_discord_request() {
-        let signature = "bee13572331740d7b68d81fd42c0c3a90c74a34648690f59088235607109397d1af696d24623eaca20e4c98ed375874ff99af491819727581caed6902bbb300f";
+        let signature = "e926a0cba63cfa65588ae268dd055877276d6d4432b2dbb30882156b8fef4fd68706c46c303b9b23a9091a700d8e26e698fd9e979701f10fbf7d358a7adcba07";
         let timestamp = "1746297636";
 
         let body = r#"{"app_permissions":"562949953601536","application_id":"1096551423958855810","attachment_size_limit":524288000,"authorizing_integration_owners":{},"entitlements":[],"id":"0000000001","token":"example_token","type":1,"user":{"avatar":"c6a249645d46209f337279cd2ca998c7","avatar_decoration_data":null,"bot":true,"clan":null,"collectibles":null,"discriminator":"0000","global_name":"Discord","id":"643945264868098049","primary_guild":null,"public_flags":1,"system":true,"username":"discord"},"version":1}"#;
 
-        let public_key = "31772c1bf4464dad0905b4f12cfb02ed1628a7ff32e84f547b6938fbacd4a5fc";
+        let public_key = "f918b1bcd0ada9f101fb43d7c8412e0e9208f4f919a7f0a7e111f063513c3391";
         let result = super::verify_discord_request(public_key, signature, timestamp, body);
 
+        assert!(result.is_ok(), "Signature verification failed: {:?}", result);
     }
 }
